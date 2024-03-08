15dd37f8ffd134bf7f0109abbdcb7e94dd2743ae
kaidiloo <36606574+kaidiloo@users.noreply.github.com>
2024-03-08 14:43:29 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /*jshint esversion: 6 */
 
-let lexlang = 'en'; // English: 'en', German: 'de', Dutch: 'nl', Chinese: 'ch1', Chinese incl. English: 'ch2'
+let lexlang =  'et'; // Estonian: 'en', 'en'; // English: 'en', German: 'de', Dutch: 'nl', Chinese: 'ch1', Chinese incl. English: 'ch2'
 
 let path_imgs = './ch_items/';
 
